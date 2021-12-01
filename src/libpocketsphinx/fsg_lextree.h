72cb217b78965ad317a1dd350db4d77b528620cf
Will Monroe <monroe@duolingo.com>
2021-11-30 21:52:23 -0500

    Increase FSG_PNODE_CTXT_BVSZ

--- src/libpocketsphinx/fsg_lextree.h
+++ src/libpocketsphinx/fsg_lextree.h
@@ -8,7 +8,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -16,16 +16,16 @@
  *    distribution.
  *
  *
- * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND 
- * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
  * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
  * NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * ====================================================================
@@ -62,7 +62,7 @@ extern "C" {
  * Make it smaller (2) to save memory if your phoneset has less than
  * 64 phones.
  */
-#define FSG_PNODE_CTXT_BVSZ	4
+#define FSG_PNODE_CTXT_BVSZ	8
 
 typedef struct fsg_pnode_ctxt_s {
     uint32 bv[FSG_PNODE_CTXT_BVSZ];
@@ -77,7 +77,7 @@ typedef struct fsg_pnode_ctxt_s {
  * transition probs are distributed along the lextree: the prob at a node
  * is the max of the probs of all leaf nodes (and, hence, FSG transitions)
  * reachable from that node.
- * 
+ *
  * To conserve memory, the underlying HMMs with state-level information are
  * allocated only as needed.  Root and leaf nodes must also account for all
  * the possible phonetic contexts, with an independent HMM for each distinct
@@ -95,14 +95,14 @@ typedef struct fsg_pnode_s {
         struct fsg_pnode_s *succ;
         fsg_link_t *fsglink;
     } next;
-  
+
     /*
      * For simplicity of memory management (i.e., freeing the pnodes), all
      * pnodes allocated for all transitions out of a state are maintained in a
      * linear linked list through the alloc_next pointer.
      */
     struct fsg_pnode_s *alloc_next;
-  
+
     /*
      * The next node that is also a child of the parent of this node; NULL if
      * none.
@@ -117,7 +117,7 @@ typedef struct fsg_pnode_s {
      * This is a logs2(prob) value, and includes the language weight.
      */
     int32 logs2prob;
-  
+
     /*
      * The root and leaf positions associated with any transition have to deal
      * with multiple phonetic contexts.  However, different contexts may result
@@ -131,11 +131,11 @@ typedef struct fsg_pnode_s {
      * (For word-internal phones, this field is unused, of course.)
      */
     fsg_pnode_ctxt_t ctxt;
-  
+
     uint16 ci_ext;	/* This node's CIphone as viewed externally (context) */
     uint8 ppos;	/* Phoneme position in pronunciation */
     uint8 leaf;	/* Whether this is a leaf node */
-  
+
     /* HMM-state-level stuff here */
     hmm_context_t *ctx;
     hmm_t hmm;
@@ -177,6 +177,16 @@ typedef struct fsg_pnode_s {
      ((src)->bv[1] = (~((sub)->bv[1]) & (src)->bv[1]))  | \
      ((src)->bv[2] = (~((sub)->bv[2]) & (src)->bv[2]))  | \
      ((src)->bv[3] = (~((sub)->bv[3]) & (src)->bv[3])))
+#elif (FSG_PNODE_CTXT_BVSZ == 8)
+    #define FSG_PNODE_CTXT_SUB(src,sub) \
+    (((src)->bv[0] = (~((sub)->bv[0]) & (src)->bv[0]))  | \
+     ((src)->bv[1] = (~((sub)->bv[1]) & (src)->bv[1]))  | \
+     ((src)->bv[2] = (~((sub)->bv[2]) & (src)->bv[2]))  | \
+     ((src)->bv[3] = (~((sub)->bv[3]) & (src)->bv[3]))  | \
+     ((src)->bv[4] = (~((sub)->bv[4]) & (src)->bv[4]))  | \
+     ((src)->bv[5] = (~((sub)->bv[5]) & (src)->bv[5]))  | \
+     ((src)->bv[6] = (~((sub)->bv[6]) & (src)->bv[6]))  | \
+     ((src)->bv[7] = (~((sub)->bv[7]) & (src)->bv[7])))
 #else
     #define FSG_PNODE_CTXT_SUB(src,sub) fsg_pnode_ctxt_sub_generic((src),(sub))
 #endif
@@ -199,7 +209,7 @@ typedef struct fsg_lextree_s {
      * Similarly, right contexts for state S: If word W transitions out of S,
      * W's first CIphone is in S's {rc}.  Words transitioning into S must consider
      * these right contexts.
-     * 
+     *
      * NOTE: Words may transition into and out of S INDIRECTLY, with intermediate
      *   null transitions.
      * NOTE: Single-phone words are difficult; only SILENCE right context is
